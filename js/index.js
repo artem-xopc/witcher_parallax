e189c9e20f8a0c8ec0cdfdfeb20a8a53390c924f
artem-xopc <romanovartem.2000@mail.ru>
2023-02-12 12:31:22 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+new Swiper('.slider', {
+    speed: 2400,
+    mousewheel: {
+        enabled: true,
+        sensitivity: 2.4,
+    },
+    spaceBetween: 18,
+    parallax: true,
+    // freeMode: true
+})
